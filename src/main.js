1e81c8e06ce2cba2049ab4a00a8be208312bcd04
LRG00 <158097628@qq.com>
2019-06-10 11:44:34 +0800

    dev 分支

--- src/main.js
+++ src/main.js
@@ -7,9 +7,12 @@ import './registerServiceWorker'
 import iView from 'iview'
 import 'iview/dist/styles/iview.css'
 import './assets/scss/index.scss'
+import ElementUI from 'element-ui'
+import 'element-ui/lib/theme-chalk/index.css'
 
 Vue.config.productionTip = false
 Vue.use(iView)
+Vue.use(ElementUI)
 
 console.log(process.env.NODE_ENV)
 new Vue({
